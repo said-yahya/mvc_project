b2318bfa014b5dc18a82805185b3e94eae3e144b
Yahya Said <macbook@MacBook-Air-macbook.local>
2025-08-19 17:39:01 +0300

    home page added

--- MVC_Project/Controllers/HomeController.cs
+++ MVC_Project/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MVC_Project.Models;
+using MVC_Project.Data;
 
 namespace MVC_Project.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
-        return View();
+        // Fetch all users from the database
+        var users = _context.Users.ToList();
+        return View(users);
     }
 
     public IActionResult Privacy()
